9ea3a6af550ca6f922470e9a4cb5bf8077a68f67
JakubKrol <contact@itdotfocus.com>
2022-05-19 15:43:05 +0200

    Added prefix to all URLs.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, {json} from "express";
+import express, {json, Router} from "express";
 import cors from 'cors';
 import 'express-async-errors';
 import {handleError} from "./utils/errors";
@@ -16,7 +16,11 @@ app.use(rateLimit({
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 }));
 
-app.use('/ad', adRouter);
+const router = Router();
+
+router.use('/ad', adRouter);
+
+app.use('/api', router);
 
 app.use(handleError);
 
